f5a11ba474bfeb975113357544312b1633427250
Pranav Sharma <ps20maths@gmail.com>
2018-07-02 04:53:28 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,11 @@
-def greeting
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-end
+# # puts "Hi! Welcome to the wonderful world of Ruby programming."
+# # puts "Please enter your name so that we can greet you more personally:"
+# name = you
+#   # puts "Hi! Welcome to the wonderful world of Ruby programming."
+#   # puts "Please enter your name so that we can greet you more personally:"
+#   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+
+
+  def greeting(name)
+    puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  end
